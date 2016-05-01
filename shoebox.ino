64e3805f6b6d870e36a36e453fed776d24c08f15
Gyuhyeok <agpro@naver.com>
2016-05-01 16:32:18 +0900

    LED 2개제어

--- shoebox.ino
+++ shoebox.ino
@@ -1,17 +1,17 @@
 int REDpin = 13;
-//int GREENpin = 5;
+int GREENpin = 5;
 
 void setup() {
   pinMode(REDpin, OUTPUT);
-  //pinMode(GREENpin, OUTPUT);
+  pinMode(GREENpin, OUTPUT);
 }
 
 void loop() {
   digitalWrite(REDpin, HIGH);
-  delay(5000); // 5초간!
-  digitalWrite(REDpin,LOW);
-  delay(5000);
-  //digitalWrite(GREENpin,HIGH);
-  //delay(5000);
-  //digitalWrite(GREENpin,LOW);
+  delay(2000);
+  digitalWrite(REDpin, LOW);
+  delay(2000);
+  digitalWrite(GREENpin, HIGH);
+  delay(4000);
+  digitalWrite(GREENpin, LOW);  
   }

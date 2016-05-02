e7ce6ab097b11d90c1cac0dd6e85bd30111ccc37
Gyuhyeok <agpro@naver.com>
2016-05-02 09:27:04 +0900

    web control test

--- shoebox.ino
+++ shoebox.ino
@@ -1,7 +1,7 @@
 #include <SoftwareSerial.h>
 #define DEBUG true
  
-SoftwareSerial esp8266(0,1); // make RX Arduino line is pin 2, make TX Arduino line is pin 3.
+SoftwareSerial esp8266(2,3); // make RX Arduino line is pin 2, make TX Arduino line is pin 3.
                                         // This means that you need to connect the TX line from the esp to the Arduino's pin 2
                                         // and the RX line from the esp to the Arduino's pin 3
 

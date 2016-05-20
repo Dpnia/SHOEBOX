ecbf7f2a45cc5d239c377226c2c6dd48ecb86fe6
Junghyun <kjh4344@naver.com>
2016-05-20 16:39:56 +0900

    호호호

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'main/index'
+
   get 'say/hello'
 
   get 'say/goodbye'
@@ -7,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'main#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

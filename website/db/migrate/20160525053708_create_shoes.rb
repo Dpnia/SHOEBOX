0357386390c88a0f45cb7286c5b09510720b478f
Junghyun <kjh4344@naver.com>
2016-05-30 10:47:18 +0900

    DB수정

--- website/db/migrate/20160525053708_create_shoes.rb
+++ website/db/migrate/20160525053708_create_shoes.rb
@@ -10,10 +10,23 @@ class CreateShoes < ActiveRecord::Migration
       t.integer :odorsensitivity
       t.integer :predictedlife
       t.integer :weight
+      t.integer :warmth
 
       t.integer :usagetime, default: 0, null: false
       t.integer :preference
       t.integer :frequency, default: 0, null: false
+
+      t.boolean :jogging
+      t.boolean :casual
+      t.boolean :sports
+      t.boolean :trip
+      t.boolean :storll
+      t.boolean :official
+      t.boolean :date
+      t.boolean :business
+      t.boolean :soccer
+      t.boolean :basketball
+      t.boolean :house
 
       t.integer :user_id
       t.timestamps null: false

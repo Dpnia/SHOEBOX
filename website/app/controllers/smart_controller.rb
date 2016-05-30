f3c8faf2461ff06b3546275d838511b2e66d3967
Junghyun <kjh4344@naver.com>
2016-05-30 23:08:50 +0900

    ipaddress 추가

--- website/app/controllers/smart_controller.rb
+++ website/app/controllers/smart_controller.rb
@@ -1,6 +1,10 @@
 class SmartController < ApplicationController
   def index
-	
   end
 
+  def change
+  	current_user.ipaddress = params[:ipaddress]
+  	current_user.save
+  	render 'index'
+  end
 end

4d40a3db3be0861f71706bc232c22f352603ec17
Junghyun <kjh4344@naver.com>
2016-06-10 20:10:08 +0900

    db드랍 후 마이그레이트 ㄱㄱ

--- website/app/controllers/recommend_controller.rb
+++ website/app/controllers/recommend_controller.rb
@@ -1,46 +1,31 @@
 class RecommendController < ApplicationController
   def index 
+ 
   end
+ 
   def result
-  	@shoe = Shoe.find(current_user.id)
-
-  	@purpose = params[:choice]
-    @temperature = params[:temp1]
-    @Raining_Snowing = params
-  	@huminity = params[:humi1]
-    @recommendIndex = 0
-
-    if@shoe.@purpose == true
-    	RecommendIndex += 20
+  	@shoe = Shoe.all
+  	@temperature = params[:temp1]
+    @huminity = params[:humi1]
+  	@shoe.each do |s|
+  	  s.recommendindex = 0
+  	  if s["params[:choice]"] == true
+    	s.recommendindex += 20
+      end
+      if @temperature > 30 && s.warmth < 40
+       	s.recommendindex += 20
     
-    if@temperature>30 && @shoe.warmth<40
-        	 RecommendIndex += 20
-    
-	elsif 20<@temperature<30 && @shoe.warmth<50
-	     RecommendIndex += 20
+	  elsif 20 < @temperature < 30 && s.warmth < 50
+	    s.recommendindex += 20
 
-	elsif 10< @temperature < 20 && @shoe.warmth<60
-
-	     RecommendIndex += 20
-
-
-	elsif @temperature<10 && @shoe.warmth > 60
-	    RecommendIndex += 20
-
-
-	elsif @temperature<0 && @shoe.warmth >80
-          RecommendIndex +=20
-
-    
-
-	    	
-end
-	
-end
-	
-end
-
-    
->>>>>>> 38d9084b43ada3c470c54f25494913e925ce3416
+	  elsif 10< @temperature < 20 && s.warmth < 60
+	    s.recommendindex += 20
+	  elsif @temperature < 10 && s.warmth > 60
+	    s.recommendindex += 20
+	  elsif @temperature < 0 && s.warmth >80
+        s.recommendindex +=20
+      end
+    end
+    @shoe.save
   end
 end

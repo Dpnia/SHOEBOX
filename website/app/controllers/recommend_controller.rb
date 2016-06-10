04b2e41bbe6caa6c9949a07dd393287f71f45f5f
shinheesub <gmltjq1004@naver.com>
2016-06-10 15:55:42 +0900

    wombat

--- website/app/controllers/recommend_controller.rb
+++ website/app/controllers/recommend_controller.rb
@@ -1,5 +1,23 @@
 class RecommendController < ApplicationController
-  def index
-	
+  def index 
+  	@resultt = Wombat.crawl do
+  		base_url "https://thingspeak.com/channels/120593/feeds.json?results=1"
+  		path "/"
+
+  		headline xpath: "//h1"
+  		subheading css: "p.subheading"
+
+  		what_is({ css: ".one-half h3" }, :list)
+
+  		links do
+    		explore xpath: '//*[@class="wrapper"]/div[1]/div[1]/div[2]/ul/li[1]/a' do |e|
+      		e.gsub(/Explore/, "Love")
+    		end
+
+    	features css: '.features'
+    	enterprise css: '.enterprise'
+    	blog css: '.blog'
+   	  end
+   	end	
   end
 end

7960dede4597716ec2b3c17c3cd4daf4587986d5
Junghyun <kjh4344@naver.com>
2016-06-10 21:19:54 +0900

    정리

--- website/app/controllers/recommend_controller.rb
+++ website/app/controllers/recommend_controller.rb
@@ -7,24 +7,45 @@ class RecommendController < ApplicationController
   	
   	@temperature = params[:temp1].to_i
     @huminity = params[:humi1].to_i
-    @weather = params[:weather]
-    @purpose = params[:choice]
+    @weather = params[:weather].to_s
+    @choice = params[:choice].to_s
   	current_user.shoes.each do |s|
   	  s.recommendindex = 0
-  	  
+  	  if @choice == "jogging" && s.jogging == true
+  	  	s.recommendindex += 30
+  	  elsif @choice == "casual" && s.casual == true
+  	  	s.recommendindex += 30
+  	  elsif @choice == "sports" && s.sports == true
+  	  	s.recommendindex += 30
+  	  elsif @choice == "trip" && s.trip == true
+  	  	s.recommendindex += 30
+  	  elsif @choice == "storll" && s.storll == true
+  	  	s.recommendindex += 30
+  	  elsif @choice == "official" && s.official == true
+  	  	s.recommendindex += 30
+  	  elsif @choice == "date" && s.date == true
+  	  	s.recommendindex += 30
+  	  elsif @choice == "business" && s.business == true
+  	  	s.recommendindex += 30
+  	  elsif @choice == "soccer" && s.soccer == true
+  	  	s.recommendindex += 30
+  	  elsif @choice == "basketball" && s.basketball == true
+  	  	s.recommendindex += 30
+  	  elsif @choice == "house" && s.house == true
+  	  	s.recommendindex += 30	
+  	  end
+
       if @temperature > 30 && s.warmth <= 40
        	s.recommendindex += 20
-    
 	  elsif 20 < @temperature && 30 >= @temperature && s.warmth <= 50
 	    s.recommendindex += 20
-
 	  elsif 10 < @temperature && 20 >= @temperature && s.warmth <= 60
 	    s.recommendindex += 20
 	  elsif @temperature <= 10 && s.warmth > 60
 	    s.recommendindex += 20
 	  elsif @temperature <= 0 && s.warmth >= 80
         s.recommendindex +=20
-      end
+      end    
     s.save
     end
   end
